8a1f35796c368bcc64eb47112e7d4dab98548253
Zahra <zahra.mohamed.n7@gmail.com>
2024-04-19 09:31:35 +0800

    First commit: created maven project

---
+++ src/main/java/org/waffles/App.java
@@ -0,0 +1,13 @@
+package org.waffles;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
